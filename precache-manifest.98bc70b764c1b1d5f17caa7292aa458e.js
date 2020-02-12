93616ff55f0834ecc49112ba66e3791879e35857
Kim, Yonghan <nearkyh@naver.com>
2020-02-12 17:29:30 +0900

    Updates

---
+++ precache-manifest.98bc70b764c1b1d5f17caa7292aa458e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2c1bb87c2b220b3a238c7732a104ae",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "194808dbafdd86cc85ad",
+    "url": "/movie_app/static/css/main.82f10629.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194808dbafdd86cc85ad",
+    "url": "/movie_app/static/js/main.e36309f6.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
